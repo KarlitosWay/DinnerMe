d70bf402529723b06c4b1ee4b0463e76b92fe78b
KarlitosWay <karl_oneill@yahoo.com>
2024-02-14 16:57:42 +1000

    Added meals summary page

--- Controllers/DinnerMeController.cs
+++ Controllers/DinnerMeController.cs
@@ -25,43 +25,16 @@ namespace DinnerMe.Controllers
         public async Task<ActionResult<List<Dinner>>> GetDinners()
         {
             // Just Dinners
-            //return (await _db.dinners.ToListAsync()).OrderByDescending(d => d.name).ToList();
+            return (await _db.dinners.ToListAsync()).OrderByDescending(d => d.name).ToList();
 
-            // Dinners joined with sides, works ok
-            /*
-             SELECT "d"."Id", "d"."category", "d"."imageUrl", "d"."method", "d"."name", "d"."notes", "s"."Id", "s"."DinnerId", "s"."imageUrl", "s"."method", "s"."name", "s"."notes"
-            FROM "dinners" AS "d"
-            LEFT JOIN "sides" AS "s" ON "d"."Id" = "s"."DinnerId"
-            ORDER BY "d"."Id"
-            */
+            // Dinners joined with sides
             //return (await _db.dinners.Include(d => d.sides).ToListAsync()).OrderByDescending(d => d.name).ToList();
 
-            // Dinners joined with ingredients, bad things happen (but query works ok when run directly on database)
-            /*
-            SELECT "d"."Id", "d"."category", "d"."imageUrl", "d"."method", "d"."name", "d"."notes", "t"."dinnersId", "t"."ingredientsId", "t"."Id", "t"."imageUrl", "t"."name", "t"."quantity"
-            FROM "dinners" AS "d"
-            LEFT JOIN (
-            SELECT "d0"."dinnersId", "d0"."ingredientsId", "i"."Id", "i"."imageUrl", "i"."name", "i"."quantity"
-            FROM "DinnerIngredient" AS "d0"
-            INNER JOIN "ingredients" AS "i" ON "d0"."ingredientsId" = "i"."Id"
-            ) AS "t" ON "d"."Id" = "t"."dinnersId"
-            ORDER BY "d"."Id", "t"."dinnersId", "t"."ingredientsId"
-            */
+            // Dinners joined with ingredients
             //return (await _db.dinners.Include(d => d.ingredients).ToListAsync()).OrderByDescending(d => d.name).ToList();
 
             // Nice big join between dinners, sides and ingredients
-            /*
-            SELECT "d"."Id", "d"."category", "d"."imageUrl", "d"."method", "d"."name", "d"."notes", "s"."Id", "s"."DinnerId", "s"."imageUrl", "s"."method", "s"."name", "s"."notes", "t"."dinnersId", "t"."ingredientsId", "t"."Id", "t"."imageUrl", "t"."name", "t"."quantity"
-            FROM "dinners" AS "d"
-            LEFT JOIN "sides" AS "s" ON "d"."Id" = "s"."DinnerId"
-            LEFT JOIN (
-            SELECT "d0"."dinnersId", "d0"."ingredientsId", "i"."Id", "i"."imageUrl", "i"."name", "i"."quantity"
-            FROM "DinnerIngredient" AS "d0"
-            INNER JOIN "ingredients" AS "i" ON "d0"."ingredientsId" = "i"."Id"
-            ) AS "t" ON "d"."Id" = "t"."dinnersId"
-            ORDER BY "d"."Id", "s"."Id", "t"."dinnersId", "t"."ingredientsId"
-            */
-            return (await _db.dinners.Include(d => d.sides).Include(d => d.ingredients).ToListAsync()).OrderByDescending(d => d.name).ToList();
+            //return (await _db.dinners.Include(d => d.sides).Include(d => d.ingredients).ToListAsync()).OrderByDescending(d => d.name).ToList();
         }
 
     }
--- Data/SeedData.cs
+++ Data/SeedData.cs
@@ -10,7 +10,7 @@ namespace DinnerMe.Data
             var dinners = new Dinner[] {
                 new Dinner() {
                     name        = "Barramundi Foil Parcels",
-                    imageUrl    = "img/dinners/default.jpg",
+                    imageUrl    = "img/default_meal.jpg",
                     method      = "1. Feck it all in a bit of tin foil\n2. Feckin' cook it, like.\n3. Sure that's it now Ted.",
                     notes       = "Yummy scrummy in my tummy.",
                     ingredients = new Ingredient[] {
@@ -25,8 +25,24 @@ namespace DinnerMe.Data
                     // Note: No sides
                 },
                 new Dinner() {
+                    name        = "Big Dirty Fry-up",
+                    imageUrl    = "img/default_meal.jpg",
+                    method      = "1. Chuck it all on the bbq.\n2. Get stuck in.",
+                    notes       = "Must check if this can be null...",
+                    ingredients = new Ingredient[] {
+                        new Ingredient() {  name = "Sausages",
+                                           imageUrl = "img/ingredients/default.jpg",
+                                           quantity = 10 },
+                        new Ingredient() {  name = "Beans",
+                                           imageUrl = "img/ingredients/default.jpg",
+                                           quantity = 1 }
+                    }.ToList<Ingredient>(),
+                    category = Dinner.Category.Fish,
+                    // Note: No sides
+                },
+                new Dinner() {
                     name = "Trucha Patagonica",
-                    imageUrl = "img/dinners/default.jpg",
+                    imageUrl = "img/default_meal.jpg",
                     method = "1. Make rosti\n2.Make rosti sandwich with salmon & rocket filling\n3.Servido!",
                     notes = "Can use spinach instead of rocket or even better use a spinach/rocket mix",
                     ingredients = new Ingredient[] {
@@ -40,7 +56,7 @@ namespace DinnerMe.Data
                     category = Dinner.Category.Fish,
                     sides = new Side[] {
                         new Side() { name = "Stuffed Green Olives",
-                                     imageUrl = "img/sides/default.jpg",
+                                     imageUrl = "img/default_meal.jpg",
                                      method = "Just serve.",
                                      notes = "None",
                                      ingredients = new Ingredient[] {
@@ -50,7 +66,7 @@ namespace DinnerMe.Data
                                      }.ToList<Ingredient>()
                                    },
                         new Side() { name = "Test Second Side",
-                                     imageUrl = "img/sides/default.jpg",
+                                     imageUrl = "img/default_meal.jpg",
                                      method = "Test Method.",
                                      notes = "None",
                                      ingredients = new Ingredient[] {
